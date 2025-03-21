8247b959cd0b7232bff8dcd0fa8bef210244add6
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2025-03-21 14:06:55 +0100

    perf(pattern): use unique usize instead of PathBuf for identification

--- src/check_dir_is_project.rs
+++ src/check_dir_is_project.rs
@@ -4,21 +4,17 @@ use std::path::{Component, Path, PathBuf};
 pub fn check_dir_is_project(patterns: &[Pattern], dir: &Path) -> bool {
     let mut state = patterns
         .iter()
-        .map(|pattern| pattern.base.clone())
+        .map(|pattern| pattern.unique_identifier)
         .collect();
     drop(recursive(&mut state, dir, patterns));
 
-    // When a pattern is matched successfully it will be removed.
+    // When a pattern is matched successfully it will be removed from the state.
     // If it is still there, it never matched.
     state.is_empty()
 }
 
-/// `state` contains the patterns that still need to be matched to accept the given path as a project
-fn recursive(
-    state: &mut HashSet<Identifier>,
-    dir: &Path,
-    patterns: &[Pattern],
-) -> std::io::Result<()> {
+/// `state` contains the `unique_identifier` that still need to be matched to accept the given path as a project
+fn recursive(state: &mut HashSet<usize>, dir: &Path, patterns: &[Pattern]) -> std::io::Result<()> {
     let entries = dir
         .read_dir()?
         .filter_map(Result::ok)
@@ -29,7 +25,7 @@ fn recursive(
         let matched_patterns = patterns
             .iter()
             .filter(|pattern| pattern.matches(&path))
-            .map(|pattern| &pattern.base);
+            .map(|pattern| &pattern.unique_identifier);
         for to_be_removed in matched_patterns {
             state.remove(to_be_removed);
         }
@@ -41,7 +37,7 @@ fn recursive(
 
     let patterns = patterns
         .iter()
-        .filter(|pattern| state.contains(&pattern.base))
+        .filter(|pattern| state.contains(&pattern.unique_identifier))
         .collect::<Vec<_>>();
 
     for dir in dirs {
@@ -64,12 +60,6 @@ fn recursive(
 enum Kind {
     File,
     Directory,
-}
-
-#[derive(Debug, Hash, Clone, PartialEq, Eq)]
-struct Identifier {
-    kind: Kind,
-    raw: PathBuf,
 }
 
 #[derive(Debug, Clone)]
@@ -102,24 +92,28 @@ impl Position {
 
 #[derive(Debug, Clone)]
 pub struct Pattern {
-    base: Identifier,
+    unique_identifier: usize,
+    kind: Kind,
     position: Position,
     target: globset::GlobMatcher,
 }
 
 impl Pattern {
     pub fn many(directory: Vec<PathBuf>, file: Vec<PathBuf>) -> Vec<Self> {
+        let offset = directory.len();
         let directory = directory
             .into_iter()
-            .map(|pattern| Self::new(pattern, Kind::Directory));
+            .enumerate()
+            .map(|(index, pattern)| Self::new(index, Kind::Directory, &pattern));
         let file = file
             .into_iter()
-            .map(|pattern| Self::new(pattern, Kind::File));
+            .enumerate()
+            .map(|(index, pattern)| Self::new(offset + index, Kind::File, &pattern));
         directory.chain(file).collect()
     }
 
-    fn new(raw: PathBuf, kind: Kind) -> Self {
-        let splitted = raw
+    fn new(unique_identifier: usize, kind: Kind, pattern: &Path) -> Self {
+        let splitted = pattern
             .components()
             .filter(|component| !matches!(component, Component::Prefix(..) | Component::RootDir))
             .map(|component| {
@@ -139,7 +133,8 @@ impl Pattern {
                     .expect("invalid glob pattern")
                     .compile_matcher();
                 Self {
-                    base: Identifier { kind, raw },
+                    unique_identifier,
+                    kind,
                     position,
                     target,
                 }
@@ -152,7 +147,8 @@ impl Pattern {
             Position::Anywhere => Some(self.clone()),
             Position::Here => None,
             Position::Below { direct, below } => direct.is_match(dir).then(|| Self {
-                base: self.base.clone(),
+                unique_identifier: self.unique_identifier,
+                kind: self.kind,
                 target: self.target.clone(),
                 position: (**below).clone(),
             }),
@@ -162,7 +158,7 @@ impl Pattern {
     fn matches(&self, path: &Path) -> bool {
         match self.position {
             Position::Anywhere | Position::Here => {
-                let kind_matches = match self.base.kind {
+                let kind_matches = match self.kind {
                     Kind::File => path.is_file(),
                     Kind::Directory => path.is_dir(),
                 };
@@ -180,9 +176,9 @@ impl Pattern {
 #[test]
 fn pattern_works_anywhere() {
     let kind = Kind::File;
-    let raw: PathBuf = "**/*.rs".parse().unwrap();
-    let result = Pattern::new(raw.clone(), kind);
-    assert_eq!(result.base, Identifier { kind, raw });
+    let pattern: PathBuf = "**/*.rs".parse().unwrap();
+    let result = Pattern::new(42, kind, &pattern);
+    assert_eq!(result.kind, kind);
     assert_eq!(result.target.glob().glob(), "*.rs");
     assert!(matches!(result.position, Position::Anywhere));
 }
@@ -190,9 +186,9 @@ fn pattern_works_anywhere() {
 #[test]
 fn pattern_works_in_base() {
     let kind = Kind::File;
-    let raw: PathBuf = "*.rs".parse().unwrap();
-    let result = Pattern::new(raw.clone(), kind);
-    assert_eq!(result.base, Identifier { kind, raw });
+    let pattern: PathBuf = "*.rs".parse().unwrap();
+    let result = Pattern::new(42, kind, &pattern);
+    assert_eq!(result.kind, kind);
     assert_eq!(result.target.glob().glob(), "*.rs");
     assert!(matches!(result.position, Position::Here));
 }
@@ -200,9 +196,9 @@ fn pattern_works_in_base() {
 #[test]
 fn pattern_works_in_subdir() {
     let kind = Kind::File;
-    let raw: PathBuf = "f*o/*.rs".parse().unwrap();
-    let result = Pattern::new(raw.clone(), kind);
-    assert_eq!(result.base, Identifier { kind, raw });
+    let pattern: PathBuf = "f*o/*.rs".parse().unwrap();
+    let result = Pattern::new(42, kind, &pattern);
+    assert_eq!(result.kind, kind);
     assert_eq!(result.target.glob().glob(), "*.rs");
     if let Position::Below { direct, below } = result.position {
         assert_eq!(direct.glob().glob(), "f*o");
@@ -215,9 +211,9 @@ fn pattern_works_in_subdir() {
 #[test]
 fn pattern_works_anywhere_in_subdir() {
     let kind = Kind::File;
-    let raw: PathBuf = "f*o/**/*.rs".parse().unwrap();
-    let result = Pattern::new(raw.clone(), kind);
-    assert_eq!(result.base, Identifier { kind, raw });
+    let pattern: PathBuf = "f*o/**/*.rs".parse().unwrap();
+    let result = Pattern::new(42, kind, &pattern);
+    assert_eq!(result.kind, kind);
     assert_eq!(result.target.glob().glob(), "*.rs");
     if let Position::Below { direct, below } = result.position {
         assert_eq!(direct.glob().glob(), "f*o");
